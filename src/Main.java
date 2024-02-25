510c3cceff0a2017b9fcad7704627aea33da796d
jawaharPasu <jawaharjw@gmail.com>
2024-02-25 18:43:49 +0530

    added random pattern printer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import algorithms.FrequencyCounter;
 import algorithms.MultiplePointer;
+import algorithms.Randoms;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,7 +12,9 @@ public class Main {
         //MultiplePointer.leastSumZero(List.of(-4,-3,-1,0,2,4,5));
         //MultiplePointer.countUniqueValues(Arrays.asList(1,1,1,2,2));
         //MultiplePointer.maxSubArraySum(List.of(2,4,1,6,3,4,8,9,2,6,3,5), 4);
-        System.out.println(MultiplePointer.isSubarray("hello", "hell world"));
-        System.out.println(MultiplePointer.isSubarray("abc", "acb"));
+        //System.out.println(MultiplePointer.isSubarray("hello", "hell world"));
+        //System.out.println(MultiplePointer.isSubarray("abc", "acb"));
+        //Randoms.printPatternTriangle(7);
+        Randoms.printDiamondPattern(3);
     }
 }
---
+++ src/algorithms/Randoms.java
@@ -0,0 +1,48 @@
+package algorithms;
+
+public class Randoms {
+
+    // print the patterns like this for any given required rows
+    // 1111111
+    // 222222
+    // 33333
+    // 4444
+    // 555
+    // 66
+    // 7
+    public static void printPatternTriangle(int row) {
+        for (int i = 1 ; i <= row; i++) {
+            for (int j=0 ; j<=row-i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    // print the following pattern
+    //      *
+    //     ***
+    //    *****
+    //     ***
+    //      *
+    public static void printDiamondPattern(int row) {
+        for (int i = 0; i<=row; i++) {
+            for (int j=1; j<=row -i; j++) {
+                System.out.print(" ");
+            }
+            for (int j =1; j<= 2*i -1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = row -1; i>=0; i--) {
+            for (int j=1; j<=row -i; j++) {
+                System.out.print(" ");
+            }
+            for (int j =1; j<= 2*i -1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
