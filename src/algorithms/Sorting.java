7fa48b889141424428225e8b5e863f502dcbeb82
jawaharPasu <jawaharjw@gmail.com>
2024-03-17 19:09:32 +0530

    modified the insertion sort to stop processing if the last previous element in lesser than the current element that is considered for insertion

--- src/algorithms/Sorting.java
+++ src/algorithms/Sorting.java
@@ -36,12 +36,13 @@ public class Sorting {
     }
 
     public static Integer[] insertionSort(Integer[] arr) {
-        for (int i =0; i< arr.length-1; i++) {
-            for (int j = i+1; j>0; j--) {
-                if (arr[j-1]>arr[j]) {
-                    int swap = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = swap;
+        for (int i =1; i< arr.length-1; i++) {
+            int currentval = arr[i];
+            for (int j = i-1; j>0 && arr[j]>currentval; j--) {
+                if (arr[j]>arr[j+1]) {
+                    int swap = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = swap;
                 }
             }
         }
